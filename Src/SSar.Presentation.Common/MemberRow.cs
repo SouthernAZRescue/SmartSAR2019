e481cd90a3deef50c3f8a90e3e2b9993a8ed5410
James Hoiby <james@hoiby.com>
2020-04-26 00:42:52 -0700

    Add member row dto

---
+++ Src/SSar.Presentation.Common/MemberRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSar.Presentation.Common
+{
+    public class MemberRow
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+    }
+}
