d0ec70cb653c5027331e6756f4cb8b36bd07c3e2
James Hoiby <james@hoiby.com>
2020-04-26 00:21:55 -0700

    Add unit test projects.

--- Src/SSar.Presentation.API/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ Src/SSar.Presentation.API/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using SSar.Presentation.API.Data;
 
 namespace SSar.Presentation.API.Data.Migrations
 {
