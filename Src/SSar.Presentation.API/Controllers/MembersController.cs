482f8eff075b4de5b398821904b46b8564715b7b
James Hoiby <james@hoiby.com>
2020-04-26 00:50:34 -0700

    Add Member API controller with get.

---
+++ Src/SSar.Presentation.API/Controllers/MembersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SSar.Presentation.Common;
+
+namespace SSar.Presentation.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class MembersController : ControllerBase
+    {
+        [HttpGet]
+        public MemberRow Get()
+        {
+            return 
+                new MemberRow()
+                {
+                    FirstName = "Frodo", 
+                    MiddleName = "Alfred", 
+                    LastName = "Baggins", 
+                    Nickname = "Fro"
+                };
+        }
+    }
+}
