b369b9dd8490c58be17f41e9d598f716d0d12b5e
James Hoiby <james@hoiby.com>
2020-04-27 21:42:45 -0700

    Add PersonNameDto type.

---
+++ Src/SSar.BC.Common.Application/Dtos/PersonNameDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSar.BC.Common.Application.Dtos
+{
+    public class PersonNameDto
+    {
+        public string First { get; private set; }
+        public string Middle { get; private set; }
+        public string Last { get; private set; }
+        public string Nickname { get; private set; }
+    }
+}
