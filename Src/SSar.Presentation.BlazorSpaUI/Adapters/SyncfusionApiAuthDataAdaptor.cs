0fab2af9f2ff3e86a78eb6d18c7438fe73116ee5
James Hoiby <james@hoiby.com>
2020-05-01 18:39:13 -0700

    Stub custom data adapter with methods

---
+++ Src/SSar.Presentation.BlazorSpaUI/Adapters/SyncfusionApiAuthDataAdaptor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading.Tasks;
+using Syncfusion.Blazor;
+
+namespace SSar.Presentation.BlazorSpaUI.Adapters
+{
+    public class SyncfusionApiAuthDataAdaptor : DataAdaptor
+    {
+        /// <summary>
+        /// Performs data Read operation synchronously.
+        /// </summary>
+        public override object Read(DataManagerRequest dataManagerRequest, string key = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Performs data Read operation asynchronously.
+        /// </summary>
+            public override Task<object> ReadAsync(DataManagerRequest dataManagerRequest, string key = null)
+        {
+            throw new NotImplementedException();
+        }
+
+            /// <summary>
+            /// Performs Insert operation synchronously.
+            /// </summary>
+            public override object Insert(DataManager dataManager, object data, string key)
+        {
+            throw new NotImplementedException();
+        }
+            /// <summary>
+            /// Performs Insert operation asynchronously.
+            /// </summary>
+            public override Task<object> InsertAsync(DataManager dataManager, object data, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+            /// <summary>
+            /// Performs Remove operation synchronously.
+            /// </summary>
+            public override object Remove(DataManager dataManager, object data, string keyField, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+            /// <summary>
+            /// Performs Remove operation asynchronously..
+            /// </summary>
+            public override Task<object> RemoveAsync(DataManager dataManager, object data, string keyField, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+            /// <summary>
+            /// Performs Update operation synchronously.
+            /// </summary>
+            public override object Update(DataManager dataManager, object data, string keyField, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+            /// <summary>
+            /// Performs Update operation asynchronously.
+            /// </summary>
+            public override Task<object> UpdateAsync(DataManager dataManager, object data, string keyField, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+            /// <summary>
+            /// Performs Batch CRUD operations synchronously.
+            /// </summary>
+            public object BatchUpdate(DataManager dataManager, object changedRecords, object addedRecords, object deletedRecords, string keyField, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+            /// <summary>
+            /// Performs Batch CRUD operations asynchronously.
+            /// </summary>
+            public Task<object> BatchUpdateAsync(DataManager dataManager, object changedRecords, object addedRecords, object deletedRecords, string keyField, string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
