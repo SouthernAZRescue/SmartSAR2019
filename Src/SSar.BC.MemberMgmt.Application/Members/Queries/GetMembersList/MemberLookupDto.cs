daa42b2e497c7579d0cf7879d764297f937af19c
James Hoiby <james.hoiby@hypergolic.onmicrosoft.com>
2020-05-14 19:22:07 +0000

    Merged PR 8: Add validation attributes to MemberLookupDto.
    Add validation attributes to MemberLookupDto.
    
    Related work items: #87

--- Src/SSar.BC.MemberMgmt.Application/Members/Queries/GetMembersList/MemberLookupDto.cs
+++ Src/SSar.BC.MemberMgmt.Application/Members/Queries/GetMembersList/MemberLookupDto.cs
@@ -1,11 +1,25 @@
-﻿namespace SSar.BC.MemberMgmt.Application.Members.Queries.GetMembersList
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SSar.BC.MemberMgmt.Application.Members.Queries.GetMembersList
 {
     public class MemberLookupDto
     {
+        // Validation attributes are required for the SyncFusion validation system
+
         public int EntityId { get; set; }
+
+        [Required(ErrorMessage = "A first name is required.")]
+        [MaxLength(50)]
         public string FirstName { get; set; }
+
+        [MaxLength(50)]
         public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "A last name is required.")]
+        [MaxLength(50)]
         public string LastName { get; set; }
+
+        [MaxLength(50)]
         public string Nickname { get; set; }
     }
 }
