a0f5f9a0c7f80baeecb39c572e2704fd645b6cd3
James Hoiby <james.hoiby@hypergolic.onmicrosoft.com>
2020-05-25 15:11:34 +0000

    Merged PR 17: Add Serilog file logging
    Adds Serilog in program.cs init and creates a file sink to Logs/. Added to .gitignore to prevent caching of logs in the repo.
    
    Related work items: #83

--- Src/SSar.Presentation.API/Program.cs
+++ Src/SSar.Presentation.API/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace SSar.Presentation.API
 {
@@ -7,11 +9,37 @@ namespace SSar.Presentation.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // TODO: In future, after production is stable,
+            // set buffered: true in file writer
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    "Logs/log-host-.txt", 
+                    rollingInterval: RollingInterval.Hour,
+                    retainedFileCountLimit: 48,
+                    rollOnFileSizeLimit: true)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting SmartSAR host from SSar.Presentation.API.Program.Main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
