fe5e76895446c4ba57806e4f1691bcba3df5846f
James Hoiby <james@hoiby.com>
2020-07-08 11:40:12 -0700

    Clarify comment.

--- Src/SSar.BC.WebCams.Core/ValueTypes/Camera.cs
+++ Src/SSar.BC.WebCams.Core/ValueTypes/Camera.cs
@@ -6,20 +6,26 @@ namespace SSar.BC.WebCams.Core.ValueTypes
 {
     public class Camera
     {
+        // Issue:
         // Due to a bug in the dotnet core deserializer used in configuration
-        // binding as of June 2020, default values assigned at the property definition can't be used.
+        // binding as of June 2020, a list with non-null default property values
+        // will not get bound to a target model. For example:
         //
-        // Example:
-        //     public string Name { get; set; } = "";
+        //     public string Name { get; set; } = "";   // A definite NOPE, not gonna work.
         //
+        // Since this Camera class gets bound to a CameraGroup as a List<Camera>, this
+        // presents a problem.
+        //
+        // Solution:
         // As a workaround an explicit default constructor has been created that sets
         // the default values.
         //
         // See: 
         //    https://stackoverflow.com/questions/59929875/dependency-injection-with-options-pattern
+        //    and
         //    https://github.com/dotnet/runtime/issues/36390 
         //
-        // Don't ask me how long it took to figure this out. No, I'm not bitter. Really.
+        // Don't ask me how long it took to figure this out. I'm not bitter. Really.
 
         public Camera()
         {
